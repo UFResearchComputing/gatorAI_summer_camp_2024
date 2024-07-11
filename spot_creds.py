d7f16857cabead5638c32a45764bd43c3c63a48a
Ian Lutticken <i.lutticken@ufl.edu>
2024-07-11 14:09:26 -0400

    Fixed Spotify Loader

--- spot_creds.py
+++ spot_creds.py
@@ -1,6 +1,6 @@
 # Short module to store Spotipy credentials
 
 # Spotipy credentials
-client_id = '06dcb5266904482983b95f6f1d02bba5'
-client_secret = 'a41cadd2a54342eb98814c48d2f958f9'
-redirect_uri = 'https://github.com/UFResearchComputing/gatorAI_summer_camp_2024'
+client_id = '34452d07a7e84f8b80fa28897232e850'
+client_secret = '09f28efebefe464fa3ceef66f4a0dd18'
+redirect_uri = 'https://localhost:8888/callback'
