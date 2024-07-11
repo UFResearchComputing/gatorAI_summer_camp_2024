968b9083dbfa57ee5c940ea7f604845b131f5bf0
Matt Gitzendanner (he/him) <magitz@ufl.edu>
2024-07-11 19:28:34 -0400

    Some cleanup

--- spot_creds.py
+++
@@ -1,6 +0,0 @@
-# Short module to store Spotipy credentials
-
-# Spotipy credentials
-client_id = '06dcb5266904482983b95f6f1d02bba5'
-client_secret = 'a41cadd2a54342eb98814c48d2f958f9'
-redirect_uri = 'https://github.com/UFResearchComputing/gatorAI_summer_camp_2024'
